5ab7b98dcd61de58887521d371c5eb3343601ada
samanvitha7 <bolisettysamanvitha@gmail.com>
2025-01-12 18:20:26 +0530

    Create pro1.py

---
+++ Lab-1/pro1.py
@@ -0,0 +1,8 @@
+#program1 TO FIND THE ADDITION OF TWO NUMBERS WITH USER INPUT
+
+num1=input("enter first number:")
+num2=input("enter second number:")
+num1=int(num1)
+num2=int(num2)
+sum=num1+num2
+print("Result of addition is",sum)
